07623a5f3c76943e9982efd48a64bbff299182df
Steven Chen <Syc0308@gmail.com>
2014-05-09 16:51:11 -0400

    Added Buffer files.
    Not included in compile.
    Still need to make parse work.

--- a0/include/buffer.h
+++ a0/include/buffer.h
@@ -1,8 +1,19 @@
+#ifndef IO_H
+#define IO_H
+
 /*
  * Buffer Array
  */
-typedef char buffer[128];
+#define MAX_BUFFER_SIZE 128
+
+typedef struct {
+    int get_index, put_index;
+    char buf[MAX_BUFFER_SIZE];
+} buffer;
 
 int buffer_put(buffer b, char c);
 
 char buffer_get(buffer b);
+
+#endif
+
--- a0/src/buffer.c
+++ a0/src/buffer.c
@@ -1,6 +1,22 @@
 /*
  * 
  */
-int buffer_put(buffer b, char c);
+void buffer_put(buffer *b, char c) {
+    b->buf[b->put_index] = c;
 
-char buffer_get(buffer b);
+    b->put_index++;
+    if (b->put_index == MAX_BUFFER_SIZE) b->put_index = 0;
+}
+
+char buffer_get(buffer *b) {
+    if (b->get_index == b->put_index) {
+        return (char)0;
+    }
+
+    char c = b->buf[b->get_index];
+
+    b->get_index++;
+    if (b->get_index == MAX_BUFFER_SIZE) b->get_index = 0;
+
+    return c;
+}
